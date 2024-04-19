27aba387ca3f780c2f46cf4ef401bd0f7defdd63
Sebastian Ślipek <sebastian.slipek@hotmail.com>
2024-04-19 12:21:44 +0200

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players"></ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
